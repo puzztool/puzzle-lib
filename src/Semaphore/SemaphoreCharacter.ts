353c82eb48636fd0b481db12d4100c539ba099db
Kyle Farnung <kfarnung@outlook.com>
2018-04-15 21:55:19 -0700

    More semaphore logic
    Moved some of the UI logic down into this object, allowing it to become
    more testable.

--- src/Semaphore/SemaphoreCharacter.ts
+++ src/Semaphore/SemaphoreCharacter.ts
@@ -1,25 +1,76 @@
 import SemaphoreData from './SemaphoreData';
+import SemaphoreDegrees from './SemaphoreDegrees';
 import SemaphoreDirection from './SemaphoreDirection';
 
 class SemaphoreCharacter {
-  private _semaphore: string;
+  private _directions: SemaphoreDirection[] = [];
+  private _semaphore: string = '';
 
-  public constructor(first: SemaphoreDirection, second: SemaphoreDirection) {
-    this.setDirections(first, second);
+  public constructor(
+      first: SemaphoreDirection = SemaphoreDirection.None,
+      second: SemaphoreDirection = SemaphoreDirection.None) {
+    if (first > SemaphoreDirection.None) {
+      this.addDirection(first);
+    }
+
+    if (second > SemaphoreDirection.None) {
+      this.addDirection(second);
+    }
+  }
+
+  public get directions() {
+    return this._directions;
+  }
+
+  public set directions(value: SemaphoreDirection[]) {
+    this._directions = value.slice(0, 2);
+    this.update();
+  }
+
+  public addDirection(direction: SemaphoreDirection) {
+    this._directions.push(direction);
+
+    if (this._directions.length > 2) {
+      this._directions.splice(0, this._directions.length - 2);
+    }
+
+    this.update();
+  }
+
+  public removeDirection(direction: SemaphoreDirection) {
+    const index = this._directions.indexOf(direction);
+    if (index >= 0) {
+      this._directions.splice(index, 1);
+    }
+
+    this.update();
+  }
+
+  public hasDirection(direction: SemaphoreDirection) {
+    return this._directions.indexOf(direction) >= 0;
+  }
+
+  public getDegrees() {
+    return this._directions
+      .map((value) => SemaphoreDegrees.ToDegrees(value))
+      .sort((a, b) => a - b);
   }
 
   public clear() {
+    this._directions.length = 0;
     this._semaphore = '';
-  }
-
-  public setDirections(first: SemaphoreDirection, second: SemaphoreDirection) {
-    this.clear();
-    const lookup = SemaphoreData.instance.lookup(first, second);
-    this._semaphore = (lookup === null) ? '' : lookup;
   }
 
   public toString() {
     return this._semaphore;
+  }
+
+  private update() {
+    const combined = this._directions.reduce(
+      (previousValue, currentValue) => previousValue |= currentValue,
+      SemaphoreDirection.None);
+
+    this._semaphore = SemaphoreData.instance.lookup(combined);
   }
 }
 
--- test/semaphore.js
+++ test/semaphore.js
@@ -49,5 +49,70 @@ describe('Semaphore', function () {
       const emptyChar = new SemaphoreCharacter(SemaphoreDirection.South, SemaphoreDirection.South);
       assert.strictEqual(emptyChar.toString(), '');
     });
+
+    it('constructor', function () {
+      let char = new SemaphoreCharacter();
+      assert.strictEqual(char.toString(), '');
+      assert.strictEqual(char.directions.length, 0);
+
+      char = new SemaphoreCharacter(SemaphoreDirection.West);
+      assert.strictEqual(char.toString(), '');
+      assert.strictEqual(char.directions.length, 1);
+      assert.strictEqual(char.directions[0], SemaphoreDirection.West);
+
+      char = new SemaphoreCharacter(SemaphoreDirection.West, SemaphoreDirection.East);
+      assert.strictEqual(char.toString(), 'R');
+      assert.strictEqual(char.directions.length, 2);
+      assert.strictEqual(char.directions[0], SemaphoreDirection.West);
+      assert.strictEqual(char.directions[1], SemaphoreDirection.East);
+    });
+
+    it('addDirection/removeDirection', function () {
+      // Start with a partial match
+      const char = new SemaphoreCharacter(SemaphoreDirection.South);
+      assert.strictEqual(char.toString(), '');
+
+      // Complete the match
+      char.addDirection(SemaphoreDirection.SouthWest);
+      assert.strictEqual(char.toString(), 'A');
+
+      // Push off the oldest direction
+      char.addDirection(SemaphoreDirection.NorthEast);
+      assert.strictEqual(char.toString(), 'L');
+
+      // Specifically remove the most recent one
+      char.removeDirection(SemaphoreDirection.NorthEast);
+      assert.strictEqual(char.toString(), '');
+
+      // Add a different second direction
+      char.addDirection(SemaphoreDirection.SouthEast);
+      assert.strictEqual(char.toString(), 'N');
+
+      // Try to remove a non-existing direction
+      char.removeDirection(SemaphoreDirection.North);
+      assert.strictEqual(char.toString(), 'N');
+    });
+
+    it('getDegrees', function () {
+      const char = new SemaphoreCharacter(SemaphoreDirection.SouthWest, SemaphoreDirection.NorthEast);
+      let [ first, second ] = char.getDegrees();
+      assert.strictEqual(first, 45);
+      assert.strictEqual(second, 225);
+
+      char.clear();
+      [ first, second ] = char.getDegrees();
+      assert.strictEqual(first, undefined);
+      assert.strictEqual(second, undefined);
+
+      char.addDirection(SemaphoreDirection.NorthWest);
+      [ first, second ] = char.getDegrees();
+      assert.strictEqual(first, 315);
+      assert.strictEqual(second, undefined);
+
+      char.addDirection(SemaphoreDirection.NorthEast);
+      [ first, second ] = char.getDegrees();
+      assert.strictEqual(first, 45);
+      assert.strictEqual(second, 315);
+    });
   });
 });
