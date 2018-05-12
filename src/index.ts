0393fa5066703eabc9b5fda5d23c6135c3dd5252
Brian Beck <brian.beck@outlook.com>
2018-05-12 01:35:11 -0700

    Add Resistor Color Support

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,28 @@
-// Braille exports
 export { default as BrailleCharacter } from './Braille/BrailleCharacter';
 export { default as BrailleDot } from './Braille/BrailleDot';
 export { default as BrailleEncoding } from './Braille/BrailleEncoding';
 export { default as BrailleStream } from './Braille/BrailleStream';
 
-// Cipher exports
 export { default as AutoKeyString } from './Cipher/AutoKeyString';
 export { default as CaesarString } from './Cipher/CaesarString';
 export { default as KeyedCipherStringBase } from './Cipher/KeyedCipherStringBase';
 export { default as VigenereString } from './Cipher/VigenereString';
 
-// Common exports
 export { default as EncodingCategory } from './Common/EncodingCategory';
 export { default as EncodingEntry } from './Common/EncodingEntry';
 
-// Conversion exports
 export { default as CharacterConversion } from './Conversion/CharacterConversion';
 export { default as CharacterTableEntry } from './Conversion/CharacterTableEntry';
 export { default as CharacterAutoConvert } from './Conversion/CharacterAutoConvert';
 export { default as CharacterEncoding } from './Conversion/CharacterEncoding';
 export { default as SignificantFigures } from './Conversion/SignificantFigures';
 
-// Morse exports
 export { default as MorseCharacter } from './Morse/MorseCharacter';
 export { default as MorseEncoding } from './Morse/MorseEncoding';
 
-// Semaphore exports
+export { default as Resistor } from './Resistor/Resistor';
+export { default as ResistorColorEntry } from './Resistor/ResistorColorEntry';
+
 export { default as SemaphoreCharacter } from './Semaphore/SemaphoreCharacter';
 export { default as SemaphoreDegrees } from './Semaphore/SemaphoreDegrees';
 export { default as SemaphoreDirection } from './Semaphore/SemaphoreDirection';
